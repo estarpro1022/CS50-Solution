4452b5d65e2326d6fdbad06cb7162995cd0fab36
Kartone <estarpro1022@163.com>
2022-07-02 19:53:48 +0800

    modify scrabble.c

--- Week2/scrabble.c
+++ Week2/scrabble.c
@@ -45,51 +45,52 @@ int compute_score(string word)
     {
         if (isalpha(word[i]))
         {
-            switch (toupper(word[i]))
-            {
-            case 'A':
-            case 'E':
-            case 'I':
-            case 'L':
-            case 'N':
-            case 'O':
-            case 'R':
-            case 'S':
-            case 'T':
-            case 'U':
-                sum += 1;
-                break;
-            case 'D':
-            case 'G':
-                sum += 2;
-                break;
-            case 'B':
-            case 'C':
-            case 'M':
-            case 'P':
-                sum += 3;
-                break;
-            case 'F':
-            case 'H':
-            case 'V':
-            case 'W':
-            case 'Y':
-                sum += 4;
-                break;
-            case 'K':
-                sum += 5;
-                break;
-            case 'J':
-            case 'X':
-                sum += 8;
-                break;
-            case 'Q':
-            case 'Z':
-                sum += 10;
-                break;
-            default:
-                break;
-            }
+            sum += POINTS[toupper(word[i]) - 'A'];
+            // switch (toupper(word[i]))
+            // {
+            // case 'A':
+            // case 'E':
+            // case 'I':
+            // case 'L':
+            // case 'N':
+            // case 'O':
+            // case 'R':
+            // case 'S':
+            // case 'T':
+            // case 'U':
+            //     sum += 1;
+            //     break;
+            // case 'D':
+            // case 'G':
+            //     sum += 2;
+            //     break;
+            // case 'B':
+            // case 'C':
+            // case 'M':
+            // case 'P':
+            //     sum += 3;
+            //     break;
+            // case 'F':
+            // case 'H':
+            // case 'V':
+            // case 'W':
+            // case 'Y':
+            //     sum += 4;
+            //     break;
+            // case 'K':
+            //     sum += 5;
+            //     break;
+            // case 'J':
+            // case 'X':
+            //     sum += 8;
+            //     break;
+            // case 'Q':
+            // case 'Z':
+            //     sum += 10;
+            //     break;
+            // default:
+            //     break;
+            // }
         }
     }
     return sum;
