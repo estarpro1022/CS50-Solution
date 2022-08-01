4aed0398d139c4734be96dab387dd33256c2a0d1
Kartone <estarpro1022@163.com>
2022-08-01 23:03:18 +0800

    crud films in Internet Movie Database

---
+++ Week7/movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN ratings ON ratings.movie_id = movies.id WHERE id IN (SELECT stars.movie_id FROM stars WHERE stars.person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5;
---
+++ Week7/movies/7.sql
@@ -0,0 +1,1 @@
+SELECT title, rating FROM movies, ratings WHERE year = 2010 AND id = movie_id ORDER BY rating DESC, title;
